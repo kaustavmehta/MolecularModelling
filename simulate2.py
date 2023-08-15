c0294d651a1c7c4c47341f2df2e96d899336112d
Kaustav <kaustavmehta@gmail.com>
2023-08-15 17:12:45 +0530

    Basic function to spawn a water molecule

--- simulate2.py
+++ simulate2.py
@@ -1,49 +1,53 @@
 import numpy as np
 
-class SimulationBox:
-    """
-    Class to instantiate the bounded box within which the simulation occurs.
-    We pass in the no. of cells that would build up an overall box in terms of
-    L x B x H. Therefore to have 1000 molecules, we need the box to have
-    at least 10x10x10 cells (symmetry is not relevant).
-
-    We first need the dimensions of the unit cell that will be replicated so that
-    the box's global coordinates can be established. In doing so, alongwith the
-    dimensions of the cell, intersection bounds can be calculated.
-
-    Input: takes a cell object and spawns cells in accordance to the LxBxH stipulation.
-    """
-    pass
-
-class UnitCell:
-    """
-    Given an input XYZ coordinate file, will generate a unit cell by calculating
-    the minimum cube volume required to avoid overlap with other cells.
-    
-    For water, its rotation about the Z-axis about the oxygen atom will form a cone.
-    This cone can additionally create an approx. spherical volume around it by free
-    rotation about its tip. The resulting cube should contain this sphere.
-
-    This object has a unit information attribute that does not change, but is only read
-    in order to generate new cells that preserve important properties while still
-    introducing random spatial orientations without creating molecular overlaps.
-    """
-    
-    pass
-
 class Molecule:
-    # Pass the file path of the XYZ coordinates
-    def __init__(self, coordinates_file):
-        
-        # Read the XYZ coordinates file and create an array of strings, where each
-        # element corresponds to a new line in the file. 
-        with open(coordinates_file) as file:
-            data = file.readlines()
-        
-        # First line of the XYZ file is the number of atoms. This limits our for loop range.
-        n_atoms = int(data[0]) # String -> Int
 
-        atom_coordinates = np.zeros(n_atoms)
+    def __init__(self, atom_H1, atom_H2, atom_O, gridIndex):
+        
+        # Coordinates in the box as to where to spawn the molecule
+        self.gridIndex = gridIndex
+        gridIndex_x = self.gridIndex[0]
+        gridIndex_y = self.gridIndex[1]
+        gridIndex_z = self.gridIndex[2]
 
-        for atom in range(2, n_atoms):
-            atom_coordinates
+        self.atom_H1 = atom_H1
+        atom_H1_x = self.atom_H1[0] * gridIndex_x
+        atom_H1_y = self.atom_H1[1] * gridIndex_y
+        atom_H1_z = self.atom_H1[2] * gridIndex_z
+
+        self.atom_H2 = atom_H2
+        atom_H2_x = self.atom_H2[0] * gridIndex_x
+        atom_H2_y = self.atom_H2[1] * gridIndex_y
+        atom_H2_z = self.atom_H2[2] * gridIndex_z
+
+        self.atom_O = atom_O
+        atom_O_x = self.atom_O[0] * gridIndex_x
+        atom_O_y = self.atom_O[1] * gridIndex_y
+        atom_O_z = self.atom_O[2] * gridIndex_z
+
+
+# Read the XYZ coordinates file and create an array of strings, where each
+# element corresponds to a new line in the file. 
+
+coordinates_file = "water.xyz"
+
+with open(coordinates_file) as file:
+    data = file.readlines()
+
+coordinates = [i.split() for i in data][2:]
+
+atom_H1 = coordinates[0][1:]
+atom_O = coordinates[1][1:]
+atom_H2 = coordinates[2][1:]
+
+def Spawn(H1, H2, O, gridLocation):
+
+    H1_x, H1_y, H1_z = float(H1[0]) * gridLocation[0], float(H1[1]) * gridLocation[1], float(H1[2]) * gridLocation[2]
+    H2_x, H2_y, H2_z = float(H2[0]) * gridLocation[0], float(H2[1]) * gridLocation[1], float(H2[2]) * gridLocation[2]
+    O_x, O_y, O_z = float(O[0]) * gridLocation[0], float(O[1]) * gridLocation[1], float(O[2]) * gridLocation[2]
+
+    return [H1_x, H1_y, H1_z], [H2_x, H2_y, H2_z], [O_x, O_y, O_z]
+
+Spawn(atom_H1, atom_H2, atom_O, [1.2, 1.2, 1.2])
+Spawn(atom_H1, atom_H2, atom_O, [1.0, 1.0, 1.0])
+
